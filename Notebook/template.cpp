30c483e2fcbdf6083923eb36556d8975b97cbbeb
joacru <joacru@users.noreply.github.com>
2019-09-15 00:11:10 -0300

    Template agregado

---
+++ Notebook/template.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+#define ult(v, a) v[v.size() - a]
+#define forn(i, n) for(int i = 0; i < n; i++)
+#define fori(i, a, n) for(int i = a; i < n; i++)
+#define forrt(i, n) for(int i = n; i >= 0; i--)
+using namespace std;
