f671671dfbdb0b38af6273d73d2bbdef4e6b618f
joacru <joacru@users.noreply.github.com>
2019-09-15 16:37:55 -0300

    Template actualizado

--- Notebook/template.cpp
+++ Notebook/template.cpp
@@ -2,5 +2,6 @@
 #define ult(v, a) v[v.size() - a]
 #define forn(i, n) for(int i = 0; i < n; i++)
 #define fori(i, a, n) for(int i = a; i < n; i++)
+#define forit(i, a, n) for(int i = a; i <= n; i++)
 #define forrt(i, n) for(int i = n; i >= 0; i--)
 using namespace std;
