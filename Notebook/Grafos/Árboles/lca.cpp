acca257638259a58d5b4a5d1c8a0a2df36d70b7c
Joaquín Cruz <joacru1000yugioh@gmail.com>
2024-10-29 01:04:39 -0300

    LCA updated

--- Notebook/Grafos/Árboles/lca.cpp
+++ Notebook/Grafos/Árboles/lca.cpp
@@ -1,43 +1,44 @@
-struct arbol{
-  vector<int> ady[NODOS], lcasparse[NODOS], dfs;
-  int profundidad[NODOS];
-  bool visitado[NODOS];
-  int raiz, m;
-  void construirLCA(int nodo){
-    int t = dfs.size(), sig;
-    for(int i = 1; i <= t; i <<= 1) lcasparse[nodo].push_back(dfs[t - i]);
-    visitado[nodo] = true;
-    profundidad[nodo] = dfs.size();
-    forn(i, t){
-      if(visitado[nodo]) continue;
-      construirLCA(ady[nodo][i]);
-    }
-    dfs.pop_back();
-  }
-  void construirLCA(){
-    dfs.clear();
-    memset(visitado, false, sizeof(visitado));
-    construirLCA(raiz);
-  }
-  int lca(int a, int b){
-    if(a == b) return a;
-    if(a == raiz || b == raiz) return raiz;
-    if(profundidad[a] < profundidad[b]) swap(a, b);
-    int diferencia, index, t, valido;
-    while(profunidad[a] != profunidad[b]){
-      diferencia = profundidad[a] - profunidad[b];
-      index = floor(log2(diferencia));
-      a = lcasparse[a][index];
-    }
-    while(a != b){
-      t = lcasparse[a].size();
-      valido = -1;
-      forn(i, t){
-        if(lcasparse[a][i] == lcasparse[b][i]) break;
-        valido = i;
-      }
-      if(valido == -1) return lcasparse[a][0];
-      a = lcasparse[a][valido]; b = lcasparse[b][valido];
-    }
-  }
-}
+const int MAXN = 2e5+5;
+
+vector<int> adj[MAXN];
+
+struct LCA{
+	const int LOG = 18;
+	
+	int n,root;
+	vector<int> level;
+	vector<vector<int>> sparse;
+	
+	LCA(int _n, int _root) : n(_n), root(_root) {
+		level.resize(n+root);
+		sparse.resize(n+root,vector<int>(LOG));
+		level[root] = -1;
+		dfs(root,root);
+	}
+	
+	void dfs(int u, int p){
+		level[u] = level[p]+1;
+		sparse[u][0] = p;
+		fori(i,1,LOG) sparse[u][i] = sparse[sparse[u][i-1]][i-1];
+		for(int v : adj[u]) if(v != p) dfs(v,u);
+	}
+	
+	int lca(int u, int v){
+		if(level[u] < level[v]) swap(u,v);
+		for(int i=LOG-1; i>=0; i--){
+			if(level[u] - (1<<i) >= level[v]) u = sparse[u][i];
+		}
+		if(u == v) return u;
+		for(int i=LOG-1; i>=0; i--){
+			if(sparse[u][i] == sparse[v][i]) continue;
+			u = sparse[u][i];
+			v = sparse[v][i];
+		}
+		return sparse[u][0];
+	}
+	
+	int query(int u, int v){
+		int w = lca(u,v);
+		return level[u] + level[v] - 2*level[w];
+	}
+};
